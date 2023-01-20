99a08eafe3e03e4793367cf62ab9ccb7d9a17e94
jerson100 <juamkoo@gmail.com>
2023-01-20 06:29:12 -0500

    Invirtiendo estado al completar la animación

--- src/components/common/Board/Board.tsx
+++ src/components/common/Board/Board.tsx
@@ -7,7 +7,7 @@ import LoadingBoard from "./components/LoadingBoard";
 const Board = () => {
   const grid: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const [isCompleteShowAnimation, setIsCompleteShowAnimation] =
-    useState<boolean>(true);
+    useState<boolean>(false);
   return (
     <>
       <BoardContainerStyle
@@ -15,13 +15,13 @@ const Board = () => {
         initial="hidden"
         animate="show"
         exit="exit"
-        onAnimationComplete={() => setIsCompleteShowAnimation(false)}
+        onAnimationComplete={() => setIsCompleteShowAnimation(true)}
       >
         {grid.map((g) => (
           <Square key={g}>{g}</Square>
         ))}
       </BoardContainerStyle>
-      <LoadingBoard isLoading={isCompleteShowAnimation} />
+      <LoadingBoard isLoading={!isCompleteShowAnimation} />
     </>
   );
 };
