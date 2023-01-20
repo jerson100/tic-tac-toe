a7249224c6db2a3bbc1ae1d3266fd217070bd6cf
jerson100 <juamkoo@gmail.com>
2023-01-20 04:16:12 -0500

    Se agrega estilos al square para animarlo al hacer hover

--- src/components/common/Square/square.style.ts
+++ src/components/common/Square/square.style.ts
@@ -7,6 +7,11 @@ const SquareContainerStyle = styled.div`
   align-items: center;
   color: rgb(232, 232, 232);
   box-shadow: 0px 0px 4px 2px red;
+  cursor: pointer;
+  transition: transform 0.4s linear;
+  &:hover {
+    transform: scale(0.8);
+  }
 `;
 
 export { SquareContainerStyle };
